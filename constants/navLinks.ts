4f21807d9772fc7e4cd00ac680ef5e697e390adb
mariana <eumarianamota@gmail.com>
2026-01-19 11:38:50 -0300

    create constants

---
+++ constants/navLinks.ts
@@ -0,0 +1,9 @@
+import { RiHomeLine, RiArchive2Line, RiBriefcase2Line, RiWrenchLine, RiPhoneLine } from '@remixicon/react'
+
+export const navLinks = [
+    {url: "#", icon: RiHomeLine, title: "Início"},
+    {url: "#projects", icon: RiArchive2Line, title: "Projetos"},
+    {url: "#experience", icon: RiBriefcase2Line, title: "Experiência"},
+    {url: "#tools", icon: RiWrenchLine, title: "Ferramentas"},
+    {url: "#footer", icon: RiPhoneLine, title: "Contato"},
+]
---
+++ constants/socialLinks.ts
@@ -0,0 +1,9 @@
+import { RiInstagramLine, RiLinkedinLine, RiGithubLine, RiMailLine, RiWhatsappLine } from "@remixicon/react"
+
+export const socialLinks = [
+    {url: "https://www.instagram.com/eumariana.dev/", icon: RiInstagramLine},
+    {url: "https://wa.me/5586999641994", icon: RiWhatsappLine},
+    {url: "https://www.linkedin.com/in/eumarianamota/", icon: RiLinkedinLine},
+    {url: "https://github.com/eumarianamota", icon: RiGithubLine},
+    {url: "https://mailto:eumarianamota@gmail.com", icon: RiMailLine},
+]

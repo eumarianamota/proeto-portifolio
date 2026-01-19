65c04ff7177edc028f44569f7ebfe43719e1094c
mariana <eumarianamota@gmail.com>
2026-01-19 11:38:26 -0300

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-import { Hero } from "./_components/hero"
+import { About } from "./_components/about"
+import { Card } from "./_components/card"
+import { Experience } from "./_components/experience"
+import { Footer } from "./_components/footer"
+import { Header } from "./_components/header"
+import { Projects } from "./_components/projects"
+import { Tools } from "./_components/tools"
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-    </main>
+    <section className="bg-black w-full h-screen px-10 flex flex-col items-center">
+      <header>
+        <Header />
+      </header>
+      <main className="mt-10 flex flex-col lg:flex-row lg:mt-20">
+        <section>
+          < Card />
+        </section>
+
+        <section className="lg:ml-100">
+          < About />
+          < Projects />
+          < Experience />
+          < Tools />
+          < Footer />
+        </section>
+      </main>
+    </section>
   )
 }

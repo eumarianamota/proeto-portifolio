78ce562e8fbe618801b723855fd3f29bfcfb830e
Mariana Mota <168020952+eumarianamota@users.noreply.github.com>
2026-01-22 16:08:35 -0300

    Refactor About component layout and styles
    Updated the About component to improve structure and styling.

--- app/_components/about.tsx
+++ app/_components/about.tsx
@@ -5,26 +5,22 @@ export function About() {
         <section className="mt-10 overflow-hidden lg:w-150 xl:w-200 lg:mt-0">
             <div className="text-center lg:text-start lg:w-150 xl:w-180">
                 <h1>FRONT-END <span className="text-gray">DEVELOPER</span></h1>
-                <p className="text-gray-400 mt-5 lg:text-lg">Sou desenvolvedora Frontend e gosto de criar interfaces que sejam bonitas e, acima de tudo, funcionais, usando tecnologias como Next.js e Tailwind CSS. Além de construir projetos para a web, tenho mergulhado nos estudos de Dados porque acredito que entender os números ajuda a construir experiências melhores para o usuário.
+                <p>Sou desenvolvedora Frontend e gosto de criar interfaces que sejam bonitas e, acima de tudo, funcionais, usando tecnologias como Next.js e Tailwind CSS. Além de construir projetos para a web, tenho mergulhado nos estudos de Dados porque acredito que entender os números ajuda a construir experiências melhores para o usuário.
 
                 <br/> <br/>Estou sempre em busca de aprendizado e me sinto motivada por projetos onde eu possa unir código, design e uma boa lógica de programação.</p>
             </div>
 
             <div className="mt-10 flex flex-col gap-5 md:flex-row lg:flex-col xl:flex-row">
-                <div className="bg-orange rounded-xl px-5 py-5 hover:scale-98 hover:brightness-90 transition transform duration-300">
+                <div className="bg-orange flex flex-col gap-5 rounded-xl px-5 py-5 hover:scale-98 hover:brightness-90 transition transform duration-300">
                     < RiBookShelfLine className="size-8 text-white"/>
-                    <h3 className="mt-5 mb-5 text-2xl font-bold">CONHEÇA MEUS PROJETOS FRONT-END</h3>
+                    <h3>CONHEÇA MEUS PROJETOS FRONT-END</h3>
                     < RiArrowRightCircleLine className="size-8 text-white text-right ml-auto"/>
                 </div>
-
-                <div className="bg-green rounded-xl px-5 py-5 hover:scale-98 hover:brightness-90 transition transform duration-300">
-                    <a target="_blank" href="https://github.com/eumarianamota/data-science-studies">
-                        < RiDiceLine className="size-8 text-black"/>
-                        <h3 className="mt-5 mb-5 text-2xl font-bold text-black">UM POUCO SOBRE MEUS ESTUDOS EM DADOS</h3>
-                        < RiArrowRightCircleLine className="size-8 text-black text-right ml-auto"/>
-                    </a>
-                    
-                </div>
+                <a className="bg-green flex flex-col gap-5 rounded-xl px-5 py-5 hover:scale-98 hover:brightness-90 transition transform duration-300" target="_blank" href="https://github.com/eumarianamota/data-science-studies">
+                    < RiDiceLine className="size-8 text-black"/>
+                    <h3 className=" text-black">UM POUCO SOBRE MEUS ESTUDOS EM DADOS</h3>
+                    < RiArrowRightCircleLine className="size-8 text-black text-right ml-auto"/>
+                </a>
             </div>
         </section>
     )

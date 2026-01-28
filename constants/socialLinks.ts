41c9fcbe60bfd82ea71aadcf4cc09bc2dd3c3c2f
Mariana Mota <168020952+eumarianamota@users.noreply.github.com>
2026-01-27 21:39:30 -0300

    Update socialLinks.ts

--- constants/socialLinks.ts
+++ constants/socialLinks.ts
@@ -1,9 +1,9 @@
 import { RiInstagramLine, RiLinkedinLine, RiGithubLine, RiMailLine, RiWhatsappLine } from "@remixicon/react"
 
 export const socialLinks = [
-    {url: "https://www.instagram.com/eumariana.dev/", icon: RiInstagramLine},
-    {url: "https://wa.me/5586999641994", icon: RiWhatsappLine},
-    {url: "https://www.linkedin.com/in/eumarianamota/", icon: RiLinkedinLine},
-    {url: "https://github.com/eumarianamota", icon: RiGithubLine},
-    {url: "https://mailto:eumarianamota@gmail.com", icon: RiMailLine},
+    {url: "https://www.instagram.com/eumariana.dev/", icon: RiInstagramLine, title: "Instagram"},
+    {url: "https://www.linkedin.com/in/eumarianamota/", icon: RiLinkedinLine, title: "Linkedin"},
+    {url: "https://github.com/eumarianamota", icon: RiGithubLine, title: "Github"},
+    {url: "mailto:eumarianamota@gmail.com", icon: RiMailLine, title: "Email"},
+    {url: "https://wa.me/5586999641994", icon: RiWhatsappLine, title: "Whatsapp"},
 ]

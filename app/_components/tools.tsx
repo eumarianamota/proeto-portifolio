89d960fe09c94e39e20c22cde8560d3dc72ace5f
Mariana Mota <168020952+eumarianamota@users.noreply.github.com>
2026-01-27 21:36:27 -0300

    Update Tools component styles and animations

--- app/_components/tools.tsx
+++ app/_components/tools.tsx
@@ -4,12 +4,12 @@ import { tools } from "@/constants/tools"
 
 export function Tools() {
     return (
-        <section id="tools" className="mt-10 overflow-hidden lg:w-150 xl:w-200">
-            <h1>PRINCIPAIS <span className="text-gray">FERRAMENTAS</span></h1>
+        <section id="tools" className="pt-10 overflow-hidden lg:w-150 xl:w-200">
+            <h1 data-aos="fade-down-right">PRINCIPAIS <span className="text-gray">FERRAMENTAS</span></h1>
 
             <div className="flex flex-row justify-center flex-wrap gap-5 lg:gap-10 lg:justify-start">
                 {tools.map((item, index) => (
-                    <div key={index} className="flex items-center gap-5 w-50 cursor-default hover:scale-105 transition transform duration-300">
+                    <div data-aos="zoom-in" key={index} className="flex items-center gap-5 w-50 cursor-default hover:scale-105 transition transform duration-300">
                         <Image
                             src={item.icon}
                             alt={item.title}

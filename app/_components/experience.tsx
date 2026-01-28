441cf49ac576d7c43cfbd4530d3e83538b3f2884
Mariana Mota <168020952+eumarianamota@users.noreply.github.com>
2026-01-27 21:33:54 -0300

    Refactor Experience component for better styling
    Updated the Experience component to improve styling and animations.

--- app/_components/experience.tsx
+++ app/_components/experience.tsx
@@ -2,12 +2,12 @@ import { experiences } from "@/constants/experiences"
 
 export function Experience() {
     return (
-        <section id="experience" className="mt-10 overflow-x-hidden lg:w-150 xl:w-200">
-            <h1>MINHAS <span className="text-gray">EXPERIÊNCIAS</span></h1>
+        <section id="experience" className="pt-10 overflow-hidden lg:w-150 xl:w-200">
+            <h1 data-aos="fade-down-right">MINHAS <span className="text-gray">EXPERIÊNCIAS</span></h1>
 
             <div className="flex flex-col gap-8">
                 {experiences.map((item, index) => (
-                    <div className="cursor-default" key={index}>
+                    <div data-aos="zoom-in" className="cursor-default" key={index}>
                         <h3>{item.title} </h3>
                         <p> {item.description} </p>
                     </div>
